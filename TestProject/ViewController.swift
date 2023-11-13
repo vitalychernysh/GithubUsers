60cedcd8e1596f386adfebe3c10defe47de4b541
Vitalii Chernysh <vitalii.chernysh@yalantis.net>
2023-11-13 11:53:17 +0400

    Initial Commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Vitalii Chernysh on 13.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
